159d5364d5edae6e70f85af03458fed13507b711
biswa_rout <biswajit.cp2026@gmail.com>
2024-10-27 13:14:13 +0530

    corrected the params.id

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,4 @@
-export default async function Meeting({ params }: { params: { id: string } }) {
-  const { id } = await params
-    return <div>My Post: {id}</div>
-  }
+export default function Meeting({ params }: { params: { id: string } }) {
+  const { id } = params;
+  return <div>My Post: {id}</div>;
+}
